99a3ca3a6e49797688ac21fb3524f9fc61bf84ba
levi2000 <64925459+levi2000-hash@users.noreply.github.com>
2021-08-05 14:34:42 +0000

    Initial commit

---
+++ SearchAlgorithms.py
@@ -0,0 +1,5 @@
+def lineairSearch(listTosearch,value):
+	for i in listTosearch:
+		print(i)
+		if value == i:
+			return "Found"
---
+++ main.py
@@ -0,0 +1,5 @@
+from SearchAlgorithms import *
+
+test = ["dog","cat","fish"]
+
+print(lineairSearch(test,"cat"))
