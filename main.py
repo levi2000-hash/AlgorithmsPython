18664ef12c6084cde10282be2ec22bbad3eec6bd
levi2000 <64925459+levi2000-hash@users.noreply.github.com>
2021-08-05 14:43:02 +0000

    Added bubbleSort to sort alg file and added test to test file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from SearchAlgorithms import *
+from SortAlgorithms import *
 
 test = ["dog","cat","fish"]
+nums = [2,9,0,1,8,4,6]
 
 print(lineairSearch(test,"cat"))
+
+bubbleSort(nums)
+print(nums)
